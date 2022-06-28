580d182175a85e4d3bcbc7e3d283333999cb39d7
Francisco Cardoso <franciscoti2022@gmail.com>
2022-06-27 21:42:53 -0300

    adicionando firebase e preparando página de login

--- src/config/AppContext.js
+++ src/config/AppContext.js
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useRouter } from 'next/router';
+import {app } from '../config/firebase'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
 
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const router  = useRouter(); 
-  const [user, setUser] = useState('Francisco'); 
+  const router  = useRouter();
+  const [loading, setLoading] = useState(true) 
+  const [user, setUser] = useState('Francisco');
+
+  useEffect(()=> {
+    getUser();
+    
+    return; 
+   },[user])
+ 
+
+  async function getUser() {      
+    return await  onAuthStateChanged(auth, (usuario) => {
+      if (usuario) {
+        setLoading(false);
+        setUser(usuario);      
+        router.push("/");  
+      }else{
+        router.push("/signin");
+      }
+    })
+  }
+ 
   
   return (
     <AppContext.Provider value={{           
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import {useContext } from 'react'
 import { AppContext } from '../config/AppContext'
 
 export default function Home() {
-  const {user} = useContext(AppContext);
-  return (
+  const {user,loading} = useContext(AppContext);
+  return loading == false ?(
     <div>
       <h1 className="text-3xl font-bold text-green-500">
         Hello world! - {user}
       </h1>     
     </div>
-  )
+  ): <h4>Loading..</h4>
 }
